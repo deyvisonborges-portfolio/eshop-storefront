a80f0e1da4ec0d440b9c12e3f512ccd54b914f73
deyvisonborges <deyvisonsilvaborges@gmail.com>
2024-05-13 15:43:59 -0300

    working in headerr

--- src/layouts/store-layout/components/header/index.tsx
+++ src/layouts/store-layout/components/header/index.tsx
@@ -1,18 +1,24 @@
+"use client";
+
 import styles from "./header.module.scss";
 import Image from "next/image";
 import { BagIcon } from "@/icons/BagIcon";
 import { FavoriteIcon } from "@/icons/FavoriteIcon";
 import { IconUserBlack32 } from "@/icons/user-black-32";
 import { Search } from "../search";
-import { HeaderNavigation } from "./navigation";
 import Link from "next/link";
+import { Text } from "@/components/text";
+import useMediaQuery from "@/hooks/useDevice";
 
 export function Header() {
+  const { hidesm } = useMediaQuery();
+
   return (
     <header className={styles.container}>
       <div className={styles["content-left"]}>
         <Link href="/">
           <Image
+            className={styles.logo}
             alt="Logo Black - eShop"
             src="/svgs/logo-black.svg"
             height={44}
@@ -20,11 +26,21 @@ export function Header() {
           />
         </Link>
 
-        <HeaderNavigation />
+        {hidesm && (
+          <div className={styles["category-button"]}>
+            <Image
+              alt="Logo Black - eShop"
+              src="/svgs/menu-line-white.svg"
+              height={24}
+              width={24}
+            />
+            <Text token="body-regular-regular">Categorias</Text>
+          </div>
+        )}
       </div>
 
       <div className={styles["content-right"]}>
-        <Search />
+        {hidesm && <Search />}
         <div className={styles["action-group"]}>
           <div className={styles["action-group--icons"]}>
             <FavoriteIcon />
